079d3df000f8c7a620a3d6ce6accbd1521eee6d4
Bakatkin <bakatkin@gmail.com>
2019-06-19 14:43:43 +0300

    ДЗ #4

---
+++ Pro_Lesson_4/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        WaitNotify wn = new WaitNotify();
+        try {
+            wn.run();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
